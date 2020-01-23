70bbcca9fc1655ace39753e5364c5b3bd5b35a37
Lyle Balcom <balcomiv@gmail.com>
2020-01-23 05:27:25 -0500

    EF work for the morning

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CompanyEmployees.Extensions
@@ -19,5 +21,10 @@ namespace CompanyEmployees.Extensions
             {
 
             });
+
+        public static void
+            ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+                services.AddDbContext<RepositoryContext>(opts =>
+                    opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
     }
 }
---
+++ Entities/RepositoryContext.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options)
+    { }
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+}
